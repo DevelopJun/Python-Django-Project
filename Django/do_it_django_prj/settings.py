b483b993952cafa5c78160595cb29327be0ff457
jungho jung <llovejune@nate.com>
2021-07-03 15:18:12 +0900

    포스트 목록  페이지에 부트스트랩 적용하기

--- Django/do_it_django_prj/settings.py
+++ Django/do_it_django_prj/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'single_pages.apps.SinglePagesConfig',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
